c3354c750da74b5e676cbb0aeb4404ccff48aade
Mohammed AL-agha <m7mdagha44@gmail.com>
2024-09-24 09:47:56 +0300

    test3

--- routes/web.php
+++ routes/web.php
@@ -6,26 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 
 });
+Route::get('/test', function () {
+    return 'welcome';
 
-
-
-Route::get('/moh', function() {
-    return 'ff';
-})->name('mohammed');
-
-
-Route::get('/test', function() {
-    return 'fuck';
 });
-// Route::get('users/{id}', [UserController::class, 'index'])->name('user.index');
-// Route::get('user', 'UserController@index')->name('user');
-// Route::post('user', 'UserController@index')->name('user');
 
-
-
-Route::get('/test1', function() {
-    return 'fuck1';
-});
 
 
 
